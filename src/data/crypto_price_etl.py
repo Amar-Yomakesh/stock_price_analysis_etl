56d0123a9a0034b068d201f3b88f6405ba256a46
MasterShifu-NJC <84331385+AmarYoamakesh@users.noreply.github.com>
2023-08-17 07:44:13 +0530

    Update crypto_price_etl.py

--- src/data/crypto_price_etl.py
+++ src/data/crypto_price_etl.py
@@ -13,7 +13,6 @@ connection_obj=sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'
 def crypto_price_capture():
     crypto = requests.get(url='https://api.wazirx.com/api/v2/tickers',verify=False)
     crypto_raw_data = crypto.json()
-    print(crypto_raw_data)
 
     high_list = list()
     low_list = list()
@@ -58,9 +57,5 @@ def crypto_price_capture():
     crypto_pricing_df['id'] = pd.read_sql_query('select ifnull(max(id),0)+1 from crypto_hourly_price',connection_obj).iloc[0,0]+range(len(crypto_pricing_df))
     crypto_pricing_df.to_sql(con=connection_obj,name='crypto_hourly_price',if_exists='append',index=False,dtype={ 'high' : sqlalchemy.FLOAT, 'low' : sqlalchemy.FLOAT, 'last' : sqlalchemy.FLOAT, 
                                                                                                                 'open' : sqlalchemy.FLOAT, 'volume' : sqlalchemy.FLOAT, 'sell' : sqlalchemy.FLOAT, 'buy' : sqlalchemy.FLOAT, 'at': sqlalchemy.Integer, 'crypto_id' : sqlalchemy.String(15) })
-    with connection_obj.connect() as con:
-        result = con.execute(sqlalchemy.text("select count(*) from stocks.crypto_hourly_price"))
-        for row in result:
-            total=(row[0])
-    return ("total number of records :" + str(total))
+    return ("total number of records :" + str(crypto_pricing_df.shape(0)))
     
